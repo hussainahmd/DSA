e1bc5e883cb4fc459baf7a3b69f178382610cc9e
hussainahmd <hussainahmad4658@gmail.com>
2023-05-24 20:30:37 +0500

    u

--- Work/Lab/labAssign2.cpp
+++ Work/Lab/labAssign2.cpp
@@ -37,7 +37,7 @@ void mergeAlternate(Node *&list1, Node *&list2)
         q = current1->next;
         b = current2->next;
 
-        current1->next = a;
+        current1->next = current2;
         a->next = q;
 
         current1 = q;
