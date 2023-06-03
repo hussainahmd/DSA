e1fc2a0928e1cfb8824d33a1c68ecdfaddcb3da0
Hussain Ahmad <101217726+hussainahmd@users.noreply.github.com>
2023-06-03 23:23:43 +0500

    Update labAssign2.cpp

--- Work/Lab/labAssign2.cpp
+++ Work/Lab/labAssign2.cpp
@@ -74,7 +74,7 @@ int main()
     addLast(l1, 5);
     addLast(l1, 7);
     // addLast(l1, 17); 
-    // addLast(l1, 13); /
+    // addLast(l1, 13); 
     // addLast(l1, 11); //
 
     addLast(l2, 12);
