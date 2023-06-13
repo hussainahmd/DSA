3bb449b3c8141d0f61ec7fee4782778512c0924c
hussainahmd <hussainahmad4658@gmail.com>
2023-06-13 22:38:13 +0500

    update

--- Z/t.cpp
+++ Z/t.cpp
@@ -10,3 +10,17 @@ int main()
     
     return 0;
 }
+void binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = (l + r) / 2;
+        if (arr[mid] == x) {
+            cout << mid;
+        }
+        if (arr[mid] > x) {
+            binarySearch(arr, l, mid - 1, x);
+        }
+        binarySearch(arr, mid + 1, r, x);
+    }
+    cout << -1;
+}
